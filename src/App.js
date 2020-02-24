695752ca818b8c07423b9c04fbe75110fc5c3bd7
EntaltsevSN <stanislav@lotus-studio.co>
2020-02-24 14:48:34 +0300

    added user settings

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-const auth = firebase.auth();
+const firestore = firebase.firestore();
 
-console.log(auth);
+const users = firestore.collection('users')
+
+console.log();
 
 class App extends React.Component {
   constructor() {
@@ -27,11 +29,13 @@ class App extends React.Component {
     this.signUp = this.signUp.bind(this);
     this.signOut = this.signOut.bind(this);
     this.authListener = this.authListener.bind(this);
+    this.getCurrentUser = this.getCurrentUser.bind(this);
 
     this.state = {
       'email': '',
       'password': '',
-      'user': null
+      'user': null,
+      'currentUser': null
     }
   }
 
@@ -52,7 +56,15 @@ class App extends React.Component {
     e.preventDefault()
 
     firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(user => console.log("You're logged in!"))
+      .then(user => {
+        users.add({
+          id: user.user.uid,
+          email: user.user.email
+        })
+          .then(ref => console.log(ref.id))
+          .catch(error => console.log(error.message))
+        console.log("You're logged in!");
+      })
       .catch(error => console.log(`Error: ${error.message}`)) 
   }
   signOut = (e) => {
@@ -63,8 +75,34 @@ class App extends React.Component {
       .catch(error => console.log(`Error: ${error.message}`))
   }
 
+  getCurrentUser = () => {
+    users.get().then(snap => snap.forEach(doc => {
+      this.setState({
+        currentUser: (this.state.user.uid === doc.data().id) && {
+          docID: doc.id,
+          ...doc.data()
+        }
+      });
+    }));
+  }
+
+  updateUserData = (e) => {
+    e.preventDefault();
+
+    const newData = {
+      ...this.state.currentUser
+    };
+
+    delete newData.docID;
+    
+    users.doc(this.state.currentUser.docID).update({
+      ...newData
+    }).then(user => console.log('The data was updated'));
+  }
+
   componentDidMount() {
     this.authListener();
+    this.getCurrentUser();
   }
 
   render() {
@@ -100,6 +138,16 @@ class App extends React.Component {
             (this.state.user ? `You're logged in as ${this.state.user.email}` : `You're not logged in`)
           }
         </section>
+        {(this.state.currentUser !== null && this.state.currentUser !== false) &&
+          <form>
+            {`E-mail: ${this.state.currentUser.email}`}
+            <input type="text" name="age" onChange={e => this.setState({ currentUser: {
+              ...this.state.currentUser,
+              age: e.target.value
+            } })} />
+            <button onClick={this.updateUserData}>Update</button>
+          </form>
+        }
       </Fragment>
     );
   }
